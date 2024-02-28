c967b8ef48b4d7fc4dc8aa1e460ba6b33307ad96
extremeheat <extreme@protonmail.ch>
2024-02-28 17:19:16 -0500

    Add markdown pre-processor

--- src/tools.js
+++ src/tools.js
@@ -1,7 +1,10 @@
 const codebase = require('./tools/codebase')
 const viz = require('./tools/viz')
+const { loadPromptSync, preMarkdown } = require('./tools/mdp')
 
 module.exports = {
   makeVizForPrompt: viz.makeVizForPrompt,
-  collectGithubRepoFiles: codebase.collectGithubRepoFiles
+  collectGithubRepoFiles: codebase.collectGithubRepoFiles,
+  preMarkdown,
+  loadPromptSync
 }
