d4741fea4efeb157eec467028bb55088b1260ede
extremeheat <extreme@protonmail.ch>
2024-04-30 02:54:17 -0400

    update logging time handling

--- src/tools/logging.js
+++ src/tools/logging.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const { join } = require('path')
 const getHTML = ({ entries, date }) => fs.readFileSync(join(__dirname, 'loggingTemplate.html'), 'utf8')
+  .replace('LXL Log', 'LXL Log - ' + date)
   .replace('window.$ENTRIES', JSON.stringify(entries))
   .replace('window.$DATE', JSON.stringify(date))
 
@@ -8,14 +9,14 @@ function createHTML (log) {
   const entries = []
   for (const entry of log) {
     entries.push({
-      on: new Date(entry.date).toLocaleString(),
+      on: new Date(entry.date).toISOString(),
       role: 'user',
       model: entry.model,
       content: entry.messages ? null : [entry.system, entry.user].join('\n'),
       messages: entry.messages
     })
     entries.push({
-      on: new Date(entry.date).toLocaleString(),
+      on: new Date(entry.date).toISOString(),
       role: 'model',
       model: entry.model,
       content: entry.responses[0].content
@@ -23,7 +24,7 @@ function createHTML (log) {
   }
   return getHTML({
     entries,
-    date: new Date().toLocaleString()
+    date: new Date().toISOString()
   })
 }
 
